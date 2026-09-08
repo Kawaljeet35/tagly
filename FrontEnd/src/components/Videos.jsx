8e60ac0abf1b08b3116f4d61def00073c1f65442
Kawaljeet Singh <kawaljeet35@gmail.com>
2026-09-08 23:39:50 +0530

    Fix profile picture on Videos page

--- FrontEnd/src/components/Videos.jsx
+++ FrontEnd/src/components/Videos.jsx
@@ -1,5 +1,35 @@
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 
 export default function Videos({ handleLogout }) {
-  return <Navbar />;
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const fetchCurrentUser = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/users/me`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      );
+
+      const data = await response.json();
+      setCurrentUser(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  return (
+    <Navbar
+      handleLogout={handleLogout}
+      profilePictureUrl={currentUser?.profilePictureUrl}
+    />
+  );
 }
